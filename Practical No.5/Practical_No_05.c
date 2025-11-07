503788d36e37b251cc7aaa3fa8b846669dba4b26
253070081 <253070081@ycce.in>
2025-11-07 23:23:13 +0530

    Add files via upload

---
+++ Practical No.5/Practical_No_05.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+int data;
+struct node *left, *right;
+};
+struct node* createNode(int data) {
+struct node* newNode = (struct node*)malloc(sizeof(struct node));
+if (newNode == NULL) {
+printf("Memory allocation failed!\n");
+exit(1);
+}
+newNode->data = data;
+newNode->left = newNode->right = NULL;
+return newNode;
+}
+struct node* insert(struct node* root, int data) {
+if (root == NULL)
+return createNode(data);
+if (data < root->data)
+root->left = insert(root->left, data);
+else if (data > root->data)
+root->right = insert(root->right, data);
+return root;
+}
+void inorder(struct node* root) {
+if (root != NULL) {
+inorder(root->left);
+printf("%d ", root->data);
+inorder(root->right);
+}
+}
+void preorder(struct node* root) {
+if (root != NULL) {
+printf("%d ", root->data);
+preorder(root->left);
+preorder(root->right);
+}
+}
+void postorder(struct node* root) {
+if (root != NULL) {
+postorder(root->left);
+postorder(root->right);
+printf("%d ", root->data);
+}
+}
+void search(struct node* root, int key) {
+if (root == NULL) {
+printf("Not Found\n");
+return;
+}
+if (root->data == key) {
+printf("Found!\n");
+} else if (key < root->data) {
+search(root->left, key);
+} else {
+search(root->right, key);
+}
+}
+int main() {
+struct node* root = NULL;
+int n, val, key;
+printf("Enter number of nodes: ");
+scanf("%d", &n);
+for (int i = 0; i < n; i++) {
+printf("Enter value for node %d: ", i + 1);
+scanf("%d", &val);
+root = insert(root, val);
+}
+printf("\nInorder: ");
+inorder(root);
+printf("\nPreorder: ");
+preorder(root);
+printf("\nPostorder: ");
+postorder(root);
+printf("\n\nEnter element to search: ");
+scanf("%d", &key);
+search(root, key);
+return 0;
+}
